1b27aa93a49f421e869af63e6b1dc261a14d4ed9
Del2909 <41267024+Del2909@users.noreply.github.com>
2019-04-28 22:42:07 +0800

    commit revert

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ $(document).ready(function () {
 	// Set default amount in view. It is text input so we can handle dot and comma as decimal mark easily (need cleaning but provided by the library)
 	$("#amount").val("0");
 
+<<<<<<< HEAD
+=======
+
+    // Set default amount. In case of mosaic transfer the XEM amount works as a multiplier. (2 XEM will multiply by 2 the quantity of the mosaics you send)
+    $("#amount").val("1");
+    $("#recipient").val("TAQLC2WUNAYC5WLTXJR52KT5I6M67VLSEQITFVMV");
+    $("#namespaceId").val("lul_enterprise");
+    $("#mosaicName").val("coconut");
+    $("#privateKey").val("2ed8463c4a1b899f1cad9fad145de8a1aec1300601091512c09f750ba5758e13");
+    mosaicAmount = 5;
+	var hidebutt = document.getElementById("payment");
+	hidebutt.setAttribute("style","visibility:hidden");
+>>>>>>> parent of 1594ae0... Disabled XEM Transaction
 	/**
      * Function to update our fee in the view
      */
@@ -41,6 +54,7 @@ $(document).ready(function () {
 	/**
      * Build transaction from form data and send
      */
+<<<<<<< HEAD
 	function send() {
 		// Check form for errors
 		if(!$("#privateKey").val() || !$("#recipient").val()) return alert('Missing parameter !');
@@ -99,3 +113,134 @@ $(document).ready(function () {
 	updateFee();
 
 });
+=======
+    function attachMosaic() {
+        console.log("yyy" + mosaicAmount);
+        // Check for form errors
+        if (undefined === mosaicAmount || !nem.utils.helpers.isTextAmountValid(mosaicAmount)) return alert('Invalid amount !');
+        if (!$("#namespaceId").val() || !$("#mosaicName").val()) return alert('Missing parameter !');
+
+        // If not XEM, fetch the mosaic definition from network
+        if ($("#mosaicName").val() !== 'xem') {
+            nem.com.requests.namespace.mosaicDefinitions(endpoint, $("#namespaceId").val()).then(function (res) {
+                // Look for the mosaic definition(s) we want in the request response (Could use ["eur", "usd"] to return eur and usd mosaicDefinitionMetaDataPairs)
+                var neededDefinition = nem.utils.helpers.searchMosaicDefinitionArray(res.data, [$("#mosaicName").val()]);
+
+                // Get full name of mosaic to use as object key
+                var fullMosaicName = $("#namespaceId").val() + ':' + $("#mosaicName").val();
+
+                // Check if the mosaic was found
+                if (undefined === neededDefinition[fullMosaicName]) return alert("Mosaic not found !");
+
+                // Set mosaic definition into mosaicDefinitionMetaDataPair
+                mosaicDefinitionMetaDataPair[fullMosaicName] = {};
+                mosaicDefinitionMetaDataPair[fullMosaicName].mosaicDefinition = neededDefinition[fullMosaicName];
+
+                // Now we have the definition we can calculate quantity out of user input
+                var quantity = mosaicAmount * Math.pow(10, neededDefinition[fullMosaicName].properties[0].value);
+                console.log(mosaicAmount);
+                //var quantity = 2;			// Create a mosaic attachment
+                var mosaicAttachment = nem.model.objects.create("mosaicAttachment")($("#namespaceId").val(), $("#mosaicName").val(), quantity);
+
+                // Push attachment into transaction mosaics
+                transferTransaction.mosaics.push(mosaicAttachment);
+
+                // Calculate back the quantity to an amount to show in the view. It should be the same as user input but we double check to see if quantity is correct.
+                var totalToShow = nem.utils.format.supply(quantity, { "namespaceId": $("#namespaceId").val(), "name": $("#mosaicName").val() }, mosaicDefinitionMetaDataPair)[0] + '.' + nem.utils.format.supply(quantity, { "namespaceId": $("#namespaceId").val(), "name": $("#mosaicName").val() }, mosaicDefinitionMetaDataPair)[1];
+
+                // Push mosaic to the list in view
+                $("#mosaicList").prepend('<li>' + totalToShow + ' <small><b>' + $("#namespaceId").val() + ':' + $("#mosaicName").val() + '</b></small> </li>');
+
+                // Update the transaction fees in view
+                updateFee();
+            },
+                function (err) {
+                    alert(err);
+                });
+        } else {
+            // Calculate quantity from user input, XEM divisibility is 6
+            var quantity = nem.utils.helpers.cleanTextAmount(mosa) * Math.pow(10, 6);
+
+            // Create a mosaic attachment
+            var mosaicAttachment = nem.model.objects.create("mosaicAttachment")($("#namespaceId").val(), $("#mosaicName").val(), quantity);
+
+            // Push attachment into transaction mosaics
+            transferTransaction.mosaics.push(mosaicAttachment);
+
+            // Calculate back the quantity to an amount to show in the view. It should be the same as user input but we double check to see if quantity is correct.
+            var totalToShow = nem.utils.format.supply(quantity, { "namespaceId": $("#namespaceId").val(), "name": $("#mosaicName").val() }, mosaicDefinitionMetaDataPair)[0] + '.' + nem.utils.format.supply(quantity, { "namespaceId": $("#namespaceId").val(), "name": $("#mosaicName").val() }, mosaicDefinitionMetaDataPair)[1];
+
+            // Push mosaic to the list in view
+            $("#mosaicList").prepend('<li>' + totalToShow + ' <small><b>' + $("#namespaceId").val() + ':' + $("#mosaicName").val() + '</b></small> </li>');
+
+            // Update the transaction fees in view
+            updateFee();
+        }
+    }
+
+    // On amount change we update fee in view
+    $("#amount").on('change keyup paste', function () {
+        updateFee();
+    });
+
+    // On message change we update fee in view
+    $("#message").on('change keyup paste', function () {
+        updateFee();
+    });
+
+    // Call send function when click on send button
+    $("#send").click(function () {
+        send();
+		myFunctionButt();
+    });
+
+    // Call attachMosaic function when click on attachMosaic button
+    $("#attachMosaic").click(function () {
+        attachMosaic();
+    });
+
+    // Initialization of fees in view
+    updateFee();
+
+});
+
+
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var checkBox1 = document.getElementById("myCheck1");
+    var checkBox2 = document.getElementById("myCheck2");
+    mosaicAmount = 5;
+    console.log(mosaicAmount);
+    checkBox2.checked = false;
+    checkBox1.checked = false;
+}
+
+function myFunction1() {
+    var checkBox = document.getElementById("myCheck");
+    var checkBox1 = document.getElementById("myCheck1");
+    var checkBox2 = document.getElementById("myCheck2");
+    mosaicAmount = 10;
+    console.log(mosaicAmount);
+    checkBox2.checked = false;
+    checkBox.checked = false;
+}
+
+function myFunction2() {
+    var checkBox = document.getElementById("myCheck");
+    var checkBox1 = document.getElementById("myCheck1");
+    var checkBox2 = document.getElementById("myCheck2");
+    mosaicAmount = 20;
+    console.log(mosaicAmount);
+    checkBox.checked = false;
+    checkBox1.checked = false;
+}
+
+function myFunctionButt() {
+	var hidenode = document.getElementById("transfer");
+	var showbutt = document.getElementById("payment");
+	hidenode.setAttribute("style","visibility:hidden");
+	showbutt.setAttribute("style","visibility:visible");
+
+}
+
+>>>>>>> parent of 1594ae0... Disabled XEM Transaction
